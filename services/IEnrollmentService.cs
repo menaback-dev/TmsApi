3a96cf488faaeee489dbdf92db4838dae5e8872d
menadev <abdullahtate3@gmail.com>
2026-08-15 02:27:52 +0300

    refactor: delete obsolete files

--- services/IEnrollmentService.cs
+++
@@ -1,9 +0,0 @@
-using TmsApi.Dtos;
-
-namespace TmsApi.Services;
-
-public interface IEnrollmentService
-{
-    Task<EnrollmentResponseDto?> GetByIdAsync(int courseId, int id, CancellationToken ct = default);
-    Task<EnrollmentResponseDto> CreateAsync(int courseId, EnrollStudentRequest request, CancellationToken ct = default);
-}
